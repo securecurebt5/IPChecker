60d0c33c5f9e5d6dfc44c91d0422bb8e9fdb154a
Boumediene Kaddour <snboumediene@gmail.com>
2015-09-20 16:33:51 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+import distutils
+distutils.setup(
+	name="IPChecker",
+	version="1.0",
+	author = "Boumediene Kaddour",
+	author_email="snboumediene@gmail.com",
+	packages=["IPChecker"],
+	scripts=[],
+	url='http://pypi.python.org/pypi/IPchecker',
+	license="LICENSE.txt",
+	description="Check if IP is Private, Public or INVALID";
+	long_description=open('README.rst').read(),
+	install_requires=[],)
